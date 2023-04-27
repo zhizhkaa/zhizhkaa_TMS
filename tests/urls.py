e845dbf516a013fabe01742cfb38be3c270636f9
Антон Костин <xxxtonchxxx@gmail.com>
2023-04-27 21:51:14 +0300

    Дорабатывал вид тест-кейсов

--- tests/urls.py
+++ tests/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path('search', views.projects_search_results, name='search_results'),
     path('project/<int:project_pk>/', views.project_view, name='project_view'),
+    path('/project/<int:project_pk>/<int:testCase_pk>/', views.testCase_view, name='testCase_view'),
 ]
